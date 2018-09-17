61b420b4a37bd0eab65b1e37eb5d9dda22e6c217
cking20 <cking20@oswego.edu>
2018-09-17 00:45:25 -0400

    deploy

--- src/main/java/com/kinglogic/dah/ServerApp.java
+++ src/main/java/com/kinglogic/dah/ServerApp.java
@@ -43,9 +43,9 @@ public class ServerApp {
             //return JSON
             before("/*", (request, response) -> {
                 response.type("application/json");
-                response.header("Access-Control-Allow-Origin", "http://localhost:8080");
+//                response.header("Access-Control-Allow-Origin", "http://localhost:8080");
                 response.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//                response.header("Access-Control-Allow-Origin", "*");
+                response.header("Access-Control-Allow-Origin", "*");
                 response.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
                 response.header("Access-Control-Allow-Credentials", "true");
                 //set up their session 
